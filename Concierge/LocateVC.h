84e5e6d771422814b9a6b092c19e35c72f894130
Fellipe Marcondes <fellipecoelho@live.com>
2014-02-04 10:20:29 -0200

    no message

---
+++ Concierge/LocateVC.h
@@ -0,0 +1,13 @@
+//
+//  LocateVC.h
+//  Concierge
+//
+//  Created by Fellipe Marcondes on 04/02/14.
+//  Copyright (c) 2014 Pathfinders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocateVC : UIViewController
+
+@end
