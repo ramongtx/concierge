f8b181b1019fdbe7f99c1fffc25dc47cc750c686
ramongtx <ramongtx@gmail.com>
2014-02-05 10:52:28 -0200

    no message

--- Concierge/Concierge/ReservationVC.m
+++ Concierge/Concierge/ReservationVC.m
@@ -9,6 +9,11 @@
 #import "ReservationVC.h"
 
 @interface ReservationVC ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *durationTextField;
+@property (weak, nonatomic) IBOutlet UITextView *detailsTextView;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
 
 @end
 
@@ -33,6 +38,18 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)submit:(id)sender {
+}
+
+- (IBAction)dateSelected:(id)sender {
+}
+
+-(void) saveReservation
+{
+    [RESERVATION setDate:[self.datePicker date]];
+    [RESERVATION setDetails:[self.detailsTextView text]];
 }
 
 @end
